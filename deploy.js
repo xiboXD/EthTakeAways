c2b3aa1fc5931a47a7f83fef0c85c838a129a942
xibo <xibo@portkey.finance>
2023-09-09 01:06:21 +0800

    feat: takeaways

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'bubble vapor dial ceiling buffalo arch robot hair mosquito document noble rely',
+  // remember to change this to your own phrase!
+  'https://sepolia.infura.io/v3/2fe3820507524090a0f4e257eff8dc1b'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Xibo'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
